b52b27e91a840ece5ade6605f17f17927811ec6c
Olli Paloviita <olli.paloviita@gmail.com>
2021-10-20 16:42:54 +0300

    listen_sync and listen_async features implemented
    Now has more parameters to define behaviour, for example can now choose between sequental and non-sequental modes

--- sshkeyboard.py
+++ sshkeyboard.py
@@ -1,78 +1,28 @@
 import asyncio
+import concurrent.futures
 import fcntl
-import sys
 import os
-import tty
+import sys
 import termios
-from time import time
+import tty
+import traceback
+
 from contextlib import contextmanager
+from time import time, sleep
+from types import SimpleNamespace
 
+# Global state
 
-async def listen_async(
-    on_press,
-    on_release,
-    until="esc",
-    lower=True,
-    debug=False,
-    sleep=0.05,
-):
-    with _raw(sys.stdin), _nonblocking(sys.stdin):
-        while _should_listen(on_press, on_release, until, lower, debug):
-            await asyncio.sleep(sleep)
+# Makes sure only listener can be started at a time
+_running = False
+# Makes sure listener stops if error has been raised
+# inside thread pool excecutor or asyncio task
+_has_not_raised_errors = True
 
-
-def listen_sync(
-    on_press,
-    on_release,
-    until="esc",
-    lower=True,
-    debug=False,
-):
-    with _raw(sys.stdin), _nonblocking(sys.stdin):
-        while _should_listen(on_press, on_release, until, lower, debug):
-            pass
-
-
-# Raw and _nonblocking inspiration from: http://ballingt.com/_nonblocking-stdin-in-python-3/
-@contextmanager
-def _raw(stream):
-    fd = stream.fileno()
-    original_stty = termios.tcgetattr(stream)
-    try:
-        tty.setcbreak(stream)
-        yield
-    finally:
-        termios.tcsetattr(stream, termios.TCSANOW, original_stty)
-
-
-@contextmanager
-def _nonblocking(stream):
-    fd = stream.fileno()
-    orig_fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-    try:
-        fcntl.fcntl(fd, fcntl.F_SETFL, orig_fl | os.O_NONBLOCK)
-        yield
-    finally:
-        fcntl.fcntl(fd, fcntl.F_SETFL, orig_fl)
-
-
-def _read_chars(amount):
-    try:
-        return sys.stdin.read(amount)
-    except IOError:
-        return None
-
-
-# Global variables to share state between sync and async versions.
-# Really the only option to access each loop cleanly.
-_press_time = time()
-_initial_press_time = time()
-_previous = ""
-_current = ""
-_ANSI_START = "\x1b"
-# All possible characters here: https://github.com/prompt-toolkit/python-prompt-toolkit/blob/master/prompt_toolkit/input/ansi_escape_sequences.py
-# This supports only the simple ones for now
-_ANSI_CHARS = {
+# All possible characters here:
+# https://github.com/prompt-toolkit/python-prompt-toolkit/blob/master/prompt_toolkit/input/ansi_escape_sequences.py
+# Listener does not support modifier keys for now
+_ANSI_CHAR_TO_READABLE = {
     # 'Regular' characters
     "\x1b": "esc",
     "\x7f": "backspace",
@@ -140,11 +90,165 @@ _ANSI_CHARS = {
     "\x1b[23;2~": "f23",
     "\x1b[24;2~": "f24",
 }
+
 _CHAR_TO_READABLE = {
     "\t": "tab",
     "\n": "enter",
     " ": "space",
 }
+
+
+def listen_sync(
+    on_press,
+    on_release,
+    until="esc",
+    sequental=False,
+    delay_second_char=0.75,
+    delay_others=0.05,
+    lower=True,
+    debug=False,
+    thread_pool_max_workers=None,
+):
+    assert not _running, "Only one listener allowed at a time"
+    assert not asyncio.iscoroutinefunction(
+        on_press
+    ), "Use listen_async if you have async on_press"
+    assert not asyncio.iscoroutinefunction(
+        on_release
+    ), "Use listen_async if you have async on_release"
+
+    asyncio.run(
+        listen_async(
+            on_press,
+            on_release,
+            until,
+            sequental,
+            delay_second_char,
+            delay_others,
+            lower,
+            debug,
+            thread_pool_max_workers,
+            sleep=None,
+        )
+    )
+
+
+async def listen_async(
+    on_press,
+    on_release,
+    until="esc",
+    sequental=False,
+    delay_second_char=0.75,
+    delay_others=0.05,
+    lower=True,
+    debug=False,
+    thread_pool_max_workers=None,
+    sleep=0.05,
+):
+    global _running
+    global _has_not_raised_errors
+    _running = True
+    _has_not_raised_errors = True
+
+    # Create thread pool executor only if it will get used
+    if not asyncio.iscoroutinefunction(on_press) or asyncio.iscoroutinefunction(
+        on_release
+    ):
+        executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=thread_pool_max_workers
+        )
+
+    state = SimpleNamespace(
+        press_time=time(),
+        initial_press_time=time(),
+        previous="",
+        current="",
+    )
+
+    def done(task):
+        if not task.cancelled() and task.exception() is not None:
+            ex = task.exception()
+            traceback.print_exception(type(ex), ex, ex.__traceback__)
+            global _has_not_raised_errors
+            _has_not_raised_errors = False
+
+    async def on_press_callback(key):
+        if sequental:
+            if asyncio.iscoroutinefunction(on_press):
+                await on_press(key)
+            else:
+                on_press(key)
+        else:
+            if asyncio.iscoroutinefunction(on_press):
+                task = asyncio.create_task(on_press(key))
+                task.add_done_callback(done)
+            else:
+                future = executor.submit(on_press, key)
+                future.add_done_callback(done)
+
+    async def on_release_callback(key):
+        if sequental:
+            if asyncio.iscoroutinefunction(on_release):
+                await on_release(key)
+            else:
+                on_release(key)
+        else:
+            if asyncio.iscoroutinefunction(on_release):
+                task = asyncio.create_task(on_release(key))
+                task.add_done_callback(done)
+            else:
+                future = executor.submit(on_release, key)
+                future.add_done_callback(done)
+
+    with _raw(sys.stdin), _nonblocking(sys.stdin):
+        while _has_not_raised_errors:
+            should_stop, state = await _react_to_key_input(
+                state,
+                on_press_callback,
+                on_release_callback,
+                until,
+                delay_second_char,
+                delay_others,
+                lower,
+                debug,
+            )
+            if should_stop:
+                break
+            if sleep is not None:
+                await asyncio.sleep(sleep)
+
+    executor.shutdown()
+    _running = False
+
+
+# Raw and _nonblocking inspiration from: http://ballingt.com/_nonblocking-stdin-in-python-3/
+@contextmanager
+def _raw(stream):
+    fd = stream.fileno()
+    original_stty = termios.tcgetattr(stream)
+    try:
+        tty.setcbreak(stream)
+        yield
+    finally:
+        termios.tcsetattr(stream, termios.TCSANOW, original_stty)
+
+
+@contextmanager
+def _nonblocking(stream):
+    fd = stream.fileno()
+    orig_fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+    try:
+        fcntl.fcntl(fd, fcntl.F_SETFL, orig_fl | os.O_NONBLOCK)
+        yield
+    finally:
+        fcntl.fcntl(fd, fcntl.F_SETFL, orig_fl)
+
+
+def _read_chars(amount):
+    try:
+        return sys.stdin.read(amount)
+    except IOError:
+        return None
 
 
 def _is_ansi(char):
@@ -154,73 +258,71 @@ def _is_ansi(char):
 
 def _read_and_parse_ansi(char):
     char += _read_chars(5)
-    if char in _ANSI_CHARS:
-        return _ANSI_CHARS[char], char
+    if char in _ANSI_CHAR_TO_READABLE:
+        return _ANSI_CHAR_TO_READABLE[char], char
     else:
         return None, char
 
 
-def _should_listen(on_press, on_release, until, lower, debug):
-    global _previous
-    global _press_time
-    global _initial_press_time
-    global _current
-
+async def _react_to_key_input(
+    state, on_press, on_release, until, delay_second_char, delay_others, lower, debug
+):
     # Read next character
-    _current = _read_chars(1)
+    state.current = _read_chars(1)
 
     # Skip and continue if read failed
-    if _current is None:
-        return True
+    if state.current is None:
+        return False, state
 
     # Handle any character
-    elif _current != "":
+    elif state.current != "":
         # Read more if ansi character, skip and continue if unknown
-        if _is_ansi(_current):
-            _current, raw = _read_and_parse_ansi(_current)
-            if _current is None:
+        if _is_ansi(state.current):
+            state.current, raw = _read_and_parse_ansi(state.current)
+            if state.current is None:
                 if debug:
                     print(f"Non-supported ansi char: {repr(raw)}")
-                return True
+                return False, state
         # Change some character representations to readable strings
-        elif _current in _CHAR_TO_READABLE:
-            _current = _CHAR_TO_READABLE[_current]
+        elif state.current in _CHAR_TO_READABLE:
+            state.current = _CHAR_TO_READABLE[state.current]
 
         # Make lower case if requested
         if lower:
-            _current = _current.lower()
+            state.current = state.current.lower()
 
         # Stop if until character has been read
-        if _current == until:
-            _previous = ""
-            _current = ""
-            return False
+        if state.current == until:
+            state.previous = ""
+            state.current = ""
+            return True, state
 
-        # Release _previous if new pressed
-        if _previous is not "" and _current != _previous:
-            on_release(_previous)
+        # Release state.previous if new pressed
+        if state.previous is not "" and state.current != state.previous:
+            await on_release(state.previous)
 
-        # Press if new character, update _previous
-        if _current != _previous:
-            on_press(_current)
-            _initial_press_time = time()
-            _previous = _current
+        # Press if new character, update state.previous
+        if state.current != state.previous:
+            await on_press(state.current)
+            state.initial_press_time = time()
+            state.previous = state.current
 
         # Update press time
-        if _current == _previous:
-            _press_time = time()
+        if state.current == state.previous:
+            state.press_time = time()
 
     # Handle empty
-    # - Release the _previous character if nothing is read
+    # - Release the state.previous character if nothing is read
     # and enough time has passed
     # - The second character comes slower than the rest on terminal
-    elif _previous is not "" and (
-        time() - _initial_press_time > 0.75 and time() - _press_time > 0.05
+    elif state.previous is not "" and (
+        time() - state.initial_press_time > delay_second_char
+        and time() - state.press_time > delay_others
     ):
-        on_release(_previous)
-        _previous = _current
+        await on_release(state.previous)
+        state.previous = state.current
 
-    return True
+    return False, state
 
 
 if __name__ == "__main__":
