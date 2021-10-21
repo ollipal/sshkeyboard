0d575ebe3e702213297b011d33dc1076ede971b8
Olli Paloviita <olli.paloviita@gmail.com>
2021-10-21 17:15:30 +0300

    Tox and pytest based unitests added
    Now easy way to test python 3.7 - 3.10 locally.
    The tests also run on github actions.

--- tests/test_sshkeyboard.py
+++ tests/test_sshkeyboard.py
@@ -1,59 +1,85 @@
-import sys
+import sshkeyboard
 
-sys.path.append("../src")
-from sshkeyboard import listen_keyboard, listen_keyboard_async, stop_listening
+from contextlib import contextmanager
 
-print("Press 'a' multiple times to run the manual tests")
+# This overrides everything that uses sys.stdin
+# and makes _read_chars predictable
+i = -1
+
+
+def setup_testing():
+    @contextmanager
+    def _raw(stream):
+        yield
+
+    @contextmanager
+    def _nonblocking(stream):
+        yield
+
+    # Returns these characters in a sequence
+    return_chars = ("a", "a", "b", "")
+
+    def _read_chars(amount):
+        global i
+        i += 1
+        return return_chars[i % len(return_chars)]
+
+    sshkeyboard._raw = _raw
+    sshkeyboard._nonblocking = _nonblocking
+    sshkeyboard._read_chars = _read_chars
+
+
+setup_testing()
 
 
 def empty(key):
     pass
 
 
-def returns_a(key):
-    if key == "a":
-        print("SUCCESS")
-    else:
-        print("FAIL")
-    stop_listening()
+def returns_char(key):
+    if key not in return_chars:
+        throw(f"Not correct char {key}")
+    sshkeyboard.stop_listening()
 
 
 def press_listen(key):
     try:
-        listen_keyboard(empty, empty)
+        sshkeyboard.listen_keyboard(empty, empty)
         print("FAIL")
     except AssertionError:
         print("SUCCESS")
-    stop_listening()
+    sshkeyboard.stop_listening()
 
 
 def press_listen_async(key):
     try:
-        listen_keyboard_async(empty, empty)
+        sshkeyboard.listen_keyboard_async(empty, empty)
         print("FAIL")
     except AssertionError:
         print("SUCCESS")
-    stop_listening()
+    sshkeyboard.stop_listening()
 
 
-# Return value
-print("listen_keyboard pressing 'a' returns 'a'")
-listen_keyboard(returns_a, empty)
-print("listen_keyboard releasing 'a' returns 'a'")
-listen_keyboard(empty, returns_a)
-print("listen_keyboard_async pressing 'a' returns 'a'")
-listen_keyboard_async(returns_a, empty)
-print("listen_keyboard_async releasing 'a' returns 'a'")
-listen_keyboard_async(empty, returns_a)
+def test_listen_returns_char():
+    # listen_keyboard press
+    sshkeyboard.listen_keyboard(returns_char, empty)
+    # listen_keyboard relese
+    sshkeyboard.listen_keyboard(empty, returns_char)
+    # listen_keyboard press
+    sshkeyboard.listen_keyboard_async(returns_char, empty)
+    # listen_keyboard release
+    sshkeyboard.listen_keyboard_async(empty, returns_char)
 
-# Multiple running failure
-print("listen_keyboard inside listen_keyboard fails")
-listen_keyboard(press_listen, empty)
-print("listen_keyboard_async inside listen_keyboard fails")
-listen_keyboard(press_listen_async, empty)
-print("listen_keyboard inside listen_keyboard_async fails")
-listen_keyboard_async(press_listen, empty)
-print("listen_keyboard_async inside listen_keyboard_async fails")
-listen_keyboard_async(press_listen_async, empty)
 
-# TODO, assert async fails listen, test sleeps, schedule orders etc.
+def test_multiple_listens_raises_error():
+    # listen_keyboard inside listen_keyboard fails
+    sshkeyboard.listen_keyboard(press_listen, empty)
+    # listen_keyboard_async inside listen_keyboard fails
+    sshkeyboard.listen_keyboard(press_listen_async, empty)
+    # listen_keyboard inside listen_keyboard_async fails
+    sshkeyboard.listen_keyboard_async(press_listen, empty)
+    # listen_keyboard_async inside listen_keyboard_async fails
+    sshkeyboard.listen_keyboard_async(press_listen_async, empty)
+
+
+# TODO, assert async fails listen, test sleeps, schedule orders, throwing errors etc.
