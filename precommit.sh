c60e5a67bb294bb01d7e74a92c74b394695b5612
Olli Paloviita <olli.paloviita@gmail.com>
2021-10-24 11:38:41 +0300

    Github actions cleaned up a bit

--- precommit.sh
+++ precommit.sh
@@ -7,19 +7,23 @@ TOX_ENVS='py37'
 # Optionally run ruby based markdown lint locally
 # After installing ruby:
 #   gem install mdl -v 0.11.0
-#   mdl --style '.mdl-style.rb' .
 RUN_MDL=false
 
 echo "tox:"
 tox -e $TOX_ENVS --parallel
+
 echo "isort:"
 isort --settings-file pyproject.toml .
+
 echo "black:"
 black --config=pyproject.toml .
+
 echo "pflake8:"
 pflake8 --config=pyproject.toml
+
 echo "codespell:"
 codespell ./src ./tests
+
 if [ "$RUN_MDL" = true ] ; then
     echo "mdl:"
     mdl --style '.mdl-style.rb' .
